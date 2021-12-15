936a36905768e520bc9532297258aa654968e673
Artur Cygan <artur.cygan@trailofbits.com>
2021-12-15 19:37:42 +0100

    Remove upstreamed libff and sbv fixes

--- default.nix
+++ default.nix
@@ -1,19 +1,9 @@
 let
-  overlays = [(final: prev:
-    # libff doesn't build on aarch64 with the default options
-    # TODO: upstream this to nixpkgs and remove the overlay
-    if prev.stdenv.system == "aarch64-darwin" then {
-      libff = prev.libff.overrideAttrs (old: {
-        cmakeFlags = old.cmakeFlags ++ ["-DCURVE=ALT_BN128" "-DUSE_ASM=OFF"];
-      });
-    } else {}
-  )];
-
   pkgs = import (builtins.fetchTarball {
-    name = "nixpkgs-unstable-2021-12-08";
-    url = "https://github.com/nixos/nixpkgs/archive/f225322e3bea8638304adfcf415cd11de99f2208.tar.gz";
-    sha256 = "sha256:1cbl7w81h2m4as15z094jkcrgg2mdi2wnkzg2dhd6080vgic11vy";
-  }) { inherit overlays; };
+    name = "nixpkgs-unstable-2021-12-15";
+    url = "https://github.com/nixos/nixpkgs/archive/52e7900640b28d13fb8996a38ffa368ea089d1eb.tar.gz";
+    sha256 = "sha256:1109dp47lk7cn5xpfbsbmc9xsq0kmzs2k705yqz3lzs05b2fkaxd";
+  }) { };
 
   # this is not perfect for development as it hardcodes solc to 0.5.7, test suite runs fine though
   # would be great to integrate solc-select to be more flexible, improve this in future
@@ -81,20 +71,6 @@ let
   # some overrides required to build hevm on aarch64, this should disappear in the future
   aarch64HaskellPackages = pkgs.haskellPackages.override {
     overrides = self: super: {
-      # cvc4 is broken on aarch64-darwin, looks like we don't need it though,
-      # don't mark it as an explicit dependency
-      # https://github.com/NixOS/nixpkgs/blob/eac53a865b53ce9e0697bc2020f9ee444eb27e7d/pkgs/development/haskell-modules/configuration-nix.nix#L496-L511
-      # TODO: fix cln package, dependency of cvc4
-      sbv = pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.overrideCabal super.sbv (old: {
-        postPatch = ''
-          sed -i -e 's|"abc"|"${pkgs.abc-verifier}/bin/abc"|' Data/SBV/Provers/ABC.hs
-          sed -i -e 's|"boolector"|"${pkgs.boolector}/bin/boolector"|' Data/SBV/Provers/Boolector.hs
-          sed -i -e 's|"yices-smt2"|"${pkgs.yices}/bin/yices-smt2"|' Data/SBV/Provers/Yices.hs
-          sed -i -e 's|"z3"|"${pkgs.z3}/bin/z3"|' Data/SBV/Provers/Z3.hs
-
-          sed -i -e 's|\[abc, boolector, cvc4, mathSAT, yices, z3, dReal\]|[abc, boolector, cvc4, yices, z3]|' SBVTestSuite/SBVConnectionTest.hs
-        '';
-      }));
       # the hackage version doesn't build on aarch64-darwin, master is fixed
       # TODO: remove this after libBF is bumped to the next version
       libBF = super.libBF.overrideAttrs (old: {
@@ -108,7 +84,7 @@ let
     };
   };
 
-  haskellPackages = if pkgs.stdenv.system == "aarch64-darwin"
+  haskellPackages = if pkgs.stdenv.isAarch64
     then aarch64HaskellPackages
     else pkgs.haskellPackages;
 
@@ -118,7 +94,7 @@ let
     buildInputs = with haskellPackages; [
       hlint
       cabal-install
-    ] ++ pkgs.lib.optional (pkgs.stdenv.system != "aarch64-darwin") [
+    ] ++ pkgs.lib.optional (!pkgs.stdenv.isAarch64) [
       # this doesn't work due to ormolu not building
       haskell-language-server
     ];
